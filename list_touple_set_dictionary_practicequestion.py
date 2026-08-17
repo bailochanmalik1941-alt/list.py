5bfb3961cdd82a8ba243a524704d2beca7c036d3
Bailochan malik <bailochanmalik1941@gmail.com>
2026-08-17 21:58:51 +0530

    Add files via upload

---
+++ list_touple_set_dictionary_practicequestion.py
@@ -0,0 +1,21 @@
+# a={"kursi":"chair","kitab":"book","sher":"tiger"}
+# b=input("Enter your name : " )
+# print(a[b])
+
+# unique
+ #empty set
+#empty_dictionary={}
+#empty_touple=()
+#empty list=[]/list()
+a=set()
+b=input("Enter your 1st set:")
+a.add(int(b))
+b=input("Enter your 2nd set:")
+a.add(int(b))
+b=input("Enter your 3rd set:")
+a.add(int(b))
+b=input("Enter your 4th set:")
+a.add(int(b))
+b=input("Enter your 5th set:")
+a.add(int(b))
+print(a)
